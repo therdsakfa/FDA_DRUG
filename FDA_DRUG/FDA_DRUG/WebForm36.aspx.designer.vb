2228925e4b2cae8b191274667ee58df10b78ae65
Therdsak Bhummarin <therdsak.b@fsa.co.th>
2020-09-16 17:34:38 +0700

    import gpp

--- FDA_DRUG/FDA_DRUG/WebForm36.aspx.designer.vb
+++ FDA_DRUG/FDA_DRUG/WebForm36.aspx.designer.vb
@@ -410,6 +410,15 @@ Partial Public Class WebForm36
     Protected WithEvents Button31 As Global.System.Web.UI.WebControls.Button
     
     '''<summary>
+    '''Button32 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Button32 As Global.System.Web.UI.WebControls.Button
+    
+    '''<summary>
     '''GridView1 control.
     '''</summary>
     '''<remarks>
