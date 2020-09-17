ddc7f5a6f5dcfb7520f84d6e0ccf3c2d7acd19e2
kitsadPu <leem.3005@gmail.com>
2020-09-17 10:39:30 +0700

    leem

--- FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/FRM_DRUG_IMPORT_MAIN.aspx.vb
+++ FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/FRM_DRUG_IMPORT_MAIN.aspx.vb
@@ -1,5 +1,25 @@
-﻿Public Class FRM_DRUG_IMPORT_MAIN
+﻿Imports System.IO
+Imports System.Xml.Serialization
+Imports FDA_DRUG.XML_CENTER
+Imports Telerik.Web.UI
+Public Class FRM_DRUG_IMPORT_MAIN
     Inherits System.Web.UI.Page
+    Private _CLS As New CLS_SESSION
+    Private _process As String = ""
+
+    Sub RunSession()
+
+        Try
+            _CLS = Session("CLS")
+            ''นำค่า Session ใส่ ในตัวแปร _CLS
+            _process = Request.QueryString("process")           'เรียก Process ที่เราเรียก
+            '_lct_ida = Request.QueryString("lct_ida")
+            '_type = Request.QueryString("type")
+            '_process_for = Request.QueryString("process_for")
+        Catch ex As Exception
+            Response.Redirect("http://privus.fda.moph.go.th/")  'เกิด  ERROR  จะเกิดกลับมาหน้า privus
+        End Try
+    End Sub
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
@@ -7,5 +27,10 @@
 
     Protected Sub btn_add_Click(sender As Object, e As EventArgs) Handles btn_add.Click
 
+        Dim url As String = ""
+            Dim NYM As String = ""
+
+        url = "http://164.115.20.224/FDA_DRUG_IMPORT/AUTHEN/AUTHEN_GATEWAY?TOKEN=" & _CLS.TOKEN & "&DL=" & "&NYM=" & NYM
+        Response.Redirect(url)
     End Sub
 End Class
