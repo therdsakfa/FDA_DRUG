7b7f87d435380e724d54911b1016b62b3de9166d
kitsadPu <leem.3005@gmail.com>
2020-09-28 17:22:46 +0700

    L

--- FDA_DRUG/FDA_DRUG/class/DAO/DAO_DRUG_IMPORT.vb
+++ FDA_DRUG/FDA_DRUG/class/DAO/DAO_DRUG_IMPORT.vb
@@ -70,7 +70,7 @@
             For Each Me.fields In datas
             Next
         End Sub
-        Public Sub GetDataby_IDA(ByVal IDA As Integer)
+        Public Sub GetDataby_IDA(ByVal IDA As String)
 
             datas = (From p In db.FDA_DRUG_IMPORT_NYM_2s Where p.NYM2_IDA = IDA Select p)
             For Each Me.fields In datas
@@ -143,7 +143,7 @@
             For Each Me.fields In datas
             Next
         End Sub
-        Public Sub GetDataby_IDA(ByVal IDA As Integer)
+        Public Sub GetDataby_IDA(ByVal IDA As String)
 
             datas = (From p In db.FDA_DRUG_IMPORT_NYM_3s Where p.NYM3_IDA = IDA Select p)
             For Each Me.fields In datas
@@ -419,7 +419,7 @@
             Next
         End Sub
 
-        Public Sub GetDataby_IDA(ByVal IDA As Integer)
+        Public Sub GetDataby_IDA(ByVal IDA As String)
 
             datas = (From p In db.FDA_DRUG_IMPORT_UPLOADs Where p.FK_DRUG_IMPORT = IDA Select p)
             For Each Me.fields In datas
