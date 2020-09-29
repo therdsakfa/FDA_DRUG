f93c684315a921f36db751ea945ad4e70d98f256
kitsadPu <leem.3005@gmail.com>
2020-09-29 17:11:27 +0700

    L

--- FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/FRM_DRUG_IMPORT_NYM2.aspx.vb
+++ FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/FRM_DRUG_IMPORT_NYM2.aspx.vb
@@ -70,17 +70,17 @@ Public Class FRM_DRUG_IMPORT_MAIN
             Dim dao As New DAO_DRUG_IMPORT.TB_FDA_DRUG_IMPORT_NYM_2
 
 
-            'If e.CommandName = "sel" Then
-            '    dao.GetDataby_IDA(NYM2_ida)
-            'Dim tr_id As Integer = 0
-            'Try
-            '    tr_id = dao.fields.TR_ID
-            'Catch ex As Exception
+            If e.CommandName = "sel" Then
+                '    dao.GetDataby_IDA(NYM2_ida)
+                'Dim tr_id As Integer = 0
+                'Try
+                '    tr_id = dao.fields.TR_ID
+                'Catch ex As Exception
 
-            'End Try
+                'End Try
 
-            System.Web.UI.ScriptManager.RegisterStartupScript(Page, GetType(Page), "ใส่ไรก็ได้", "Popups  ('" & "POPUP_NYM_SUBMIT_REQUEST.aspx?');", True)
-            'End If
+                System.Web.UI.ScriptManager.RegisterStartupScript(Page, GetType(Page), "ใส่ไรก็ได้", "Popups  ('" & "POPUP_NYM_SUBMIT_REQUEST.aspx?');", True)
+            End If
         End If
     End Sub
 
