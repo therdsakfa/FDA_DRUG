3a7bbc0d212e7b514256a92c9aafe8fe8398609a
kitsadPu <leem.3005@gmail.com>
2020-10-01 14:06:53 +0700

    L

--- FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/POPUP_NYM_SUBMIT_REQUEST.aspx.vb
+++ FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/POPUP_NYM_SUBMIT_REQUEST.aspx.vb
@@ -194,6 +194,10 @@ Public Class POPUP_NYM_SUBMIT_REQUEST
         class_xml3.NYM_3s = dao3.fields
         class_xml4.NYM_4s = dao4.fields
 
+        If _Process = "1027" Then
+
+        End If
+
         Dim bao_show As New BAO_SHOW
         class_xml2.DT_SHOW.DT26 = bao_show.SP_LOCATION_ADDRESS_BY_IDA_NYM2(_IDA)
         class_xml3.DT_SHOW.DT25 = bao_show.SP_LOCATION_ADDRESS_BY_IDA_NYM3(_IDA)
