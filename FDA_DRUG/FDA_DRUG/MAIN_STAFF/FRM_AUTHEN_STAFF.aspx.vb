e5cd133632048ad0ef3f1c72e6fd569d11ee94e1
Therdsak Bhummarin <therdsak.b@fsa.co.th>
2020-10-05 17:14:45 +0700

    fgh

--- FDA_DRUG/FDA_DRUG/MAIN_STAFF/FRM_AUTHEN_STAFF.aspx.vb
+++ FDA_DRUG/FDA_DRUG/MAIN_STAFF/FRM_AUTHEN_STAFF.aspx.vb
@@ -11,7 +11,7 @@
     Private _TOKEN As String
     Private Sub RunQuery()
         _TOKEN = Request("Token").ToString()
-        '_TOKEN = "XYWacAxjOcdTMxIGtVOl3gUU" '--------
+        '_TOKEN = "xlQRTO2cnsiAuLBloxnJfwUU" '--------
         '_TOKEN = "N/J1pNwqJ2fJvZ/1jRPoRwUU"
     End Sub
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
