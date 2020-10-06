38ece1a49000ca8f6ec9d4ee0148f1ac195a39c9
Therdsak Bhummarin <therdsak.b@fsa.co.th>
2020-10-06 17:39:34 +0700

    xxxx

--- FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/POPUP_NYM_SUBMIT_REQUEST.aspx.vb
+++ FDA_DRUG/FDA_DRUG/D_NEW_DRUG_IMPORT/POPUP_NYM_SUBMIT_REQUEST.aspx.vb
@@ -237,8 +237,9 @@ Public Class POPUP_NYM_SUBMIT_REQUEST
         dao_nym.getdata_dl(_DL)
         Dim dao_pdftemplate As New DAO_DRUG.ClsDB_MAS_TEMPLATE_PROCESS
         Dim paths As String = bao._PATH_DEFAULT
-        Dim PDF_TEMPLATE As String = paths & "PDF_TEMPLATE\" & dao_pdftemplate.fields.PDF_TEMPLATE
+
         dao_pdftemplate.GetDataby_TEMPLAETE_and_P_ID_and_STATUS_and_PREVIEW(_process, 1, 0)
+        Dim PDF_TEMPLATE As String = paths & "PDF_TEMPLATE\" & dao_pdftemplate.fields.PDF_TEMPLATE
         Dim year As String = Date.Now.Year
         Dim filename As String = paths & dao_pdftemplate.fields.PDF_OUTPUT & "\" & NAME_PDF("DA", _process, year, dao_nym.fields.TR_ID)
         Dim Path_XML As String = paths & dao_pdftemplate.fields.XML_PATH & "\" & NAME_XML("DA", _Process, year, dao_nym.fields.TR_ID)
