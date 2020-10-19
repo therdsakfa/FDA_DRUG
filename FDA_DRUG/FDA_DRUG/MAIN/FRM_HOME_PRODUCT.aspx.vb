84d345f3297d9ae8ea964995864513ed39cd1f8f
kitsadPu <leem.3005@gmail.com>
2020-10-19 15:56:37 +0700

    L

--- FDA_DRUG/FDA_DRUG/MAIN/FRM_HOME_PRODUCT.aspx.vb
+++ FDA_DRUG/FDA_DRUG/MAIN/FRM_HOME_PRODUCT.aspx.vb
@@ -8,8 +8,8 @@
     Private _TOKEN As String
 
     Private Sub RunQuery()
-        '_TOKEN = Request("Token").ToString()
-        _TOKEN = "HDuU0SE7AJq0wr64FD5vtwUU" 'test                อย่าลืมแก้กลับ
+        _TOKEN = Request("Token").ToString()
+        '_TOKEN = "4RBzlwhkPpJjltLuXEc5nwUU" 'test                อย่าลืมแก้กลับ
         '_TOKEN = "K1JtRwgdZD5oslzpr5dLKgUU" 'AOF
     End Sub
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
