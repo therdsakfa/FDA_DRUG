6258abe95922fb68c901b6eba05f3f0fcc7bfbaa
Amornsak Yanyongdilok <amornsak.y@fsa.co.th>
2021-02-08 12:06:36 +0700

    DS

--- FDA_DRUG/FDA_DRUG/MAIN_STAFF/FRM_AUTHEN_STAFF.aspx.vb
+++ FDA_DRUG/FDA_DRUG/MAIN_STAFF/FRM_AUTHEN_STAFF.aspx.vb
@@ -11,7 +11,7 @@
     Private _TOKEN As String
     Private Sub RunQuery()
         _TOKEN = Request("Token").ToString()
-        '_TOKEN = "jB8rM2WAf/k9ATlCFgu4dgUU"    '--------
+        '_TOKEN = "jkrDAjM/ttITcsbFE/SuKwUU"    '--------
         '_TOKEN = "N/J1pNwqJ2fJvZ/1jRPoRwUU"
     End Sub
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
